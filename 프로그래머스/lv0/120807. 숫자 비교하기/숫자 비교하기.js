8576bc7f621de09a039a418fa8e539685b71eb15
Haru <103750189+minwoo02@users.noreply.github.com>
2023-09-16 23:46:27 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,7 +1,4 @@
-const solution = (num1, num2) => {
-    if(num1 !== num2) {
-        return -1
-    } else {
-        return 1
-    }
+function solution(num1, num2) {
+    const result = num1 === num2 ? 1 : -1
+    return result;
 }
