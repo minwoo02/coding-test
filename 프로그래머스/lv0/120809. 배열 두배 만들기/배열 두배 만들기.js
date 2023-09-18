e539766f8fa334f17f23463bcbf2858bed5ff68f
Haru <103750189+minwoo02@users.noreply.github.com>
2023-09-18 21:39:23 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.14 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -1,7 +1,7 @@
 function solution(numbers) {
-    let answer = numbers;
-    for(i=0; i < answer.length; i++) {
-        answer[i] = answer[i] * 2;
+    let answer = [];
+    for(i=0; i < numbers.length; i++) {
+        answer[i] = numbers[i] * 2;
     }
     return answer;
 }
